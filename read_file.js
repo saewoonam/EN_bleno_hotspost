aad99ee076a4eb0f3fe05ae92f61cbc2c5f16d60
Sae Woo <nams@nist.gov>
2020-09-20 09:40:31 -0600

    process raw data, ignore old format

--- read_file.js
+++ read_file.js
@@ -7,32 +7,76 @@ console.log(zeros.toString('hex'));
 row_buffer = Buffer.alloc(64);
 
 const readable = fs.createReadStream('588e8166afd3',null);
-readable.on('readable', () => {
+// const readable = fs.createReadStream('588e81a54819',null);
+let block = [];
+let part_one = true;
+let chunk;
+let skip = 0;
+let count = 0;
 
-    let chunk;
-    let skip = 0;
-    let part_one = true;
-    let count = 0;
+readable.on('readable', () => {
     while (null !== (chunk = readable.read(32))) {
         if (skip>0) {
             skip -= 1;
+            // console.log(skip,': ', chunk.toString('hex'))
         } else if (Buffer.compare(zeros, chunk)==0) {
-            console.log("found zeros");
             skip = 2;
+            // console.log(skip, ': ', chunk.toString('hex'))
+            part_one = true;
         } else {
-            // console.log(`read: ${chunk.length}`);
+            // console.log('start part_one', part_one)
             if (part_one) {
                 chunk.copy(row_buffer, 0, 0);
+                // console.log("part one", chunk.toString('hex'))
                 part_one = false;
             } else {
                 chunk.copy(row_buffer, 32, 0);
-                d = convert.bytesToData(row_buffer);
-                console.log(count, chunk)
-                part_one = true;
-                count += 1;
+                // console.log("part two", chunk.toString('hex'))
+                //
+                // Don't process any old data if it is there...
+                if((row_buffer[63]==0)
+                    &&(row_buffer[62]==0x07 || row_buffer[62]==0x01)) {
+                    part_one = true;
+                    // console.log("old format");
+                } else {
+                    // console.log("new format", row_buffer[63], row_buffer[62]);
+                    part_one = true;
+                    data = convert.bytesToData(row_buffer);
+                    data = data[0]
+                    if (data.minute > (1577836800/60)) {
+                        block.push(data);
+                        count += 1; 
+                    } else {
+                        // console.log("too early");
+                    }
+                    if (count==50) {
+                        let encounters = block.map(d => {
+                            return {
+                                clientKey: d.encounter_id,
+                                timestamp: d.timestamp,
+                                _meta: d
+                            }
+                        })
+                        console.log("convert block of 50: ", data.timestamp);
+                        count = 0;
+                        block = [];
+                    }
+                }
             }
+            // console.log('end part_one', part_one)
         }
     }
+});
+readable.on('close', () => {
+    console.log('closed ', block.length)
+    let encounters = block.map(d => {
+        return {
+            clientKey: d.encounter_id,
+            timestamp: d.timestamp,
+            _meta: d
+        }
+    })
+    console.log("convert last block");
 });
 
 if (false) {
@@ -48,7 +92,6 @@ if (false) {
             evt.copy(row_buffer, 0, 0)
             // console.log(row_buffer)
             d = convert.bytesToData(row_buffer);
-            console.log(d)
             i++;
         }
     }
