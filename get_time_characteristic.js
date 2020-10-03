b03c4f8c8673c29ac25dba79ac52fc1077ed409e
Sae Woo <nams@nist.gov>
2020-10-02 18:50:40 -0600

    Try to implement callback

--- get_time_characteristic.js
+++ get_time_characteristic.js
@@ -6,6 +6,11 @@ var bleno = require('bleno');
 
 var BlenoCharacteristic = bleno.Characteristic;
 
+var time_cb = function(code, value) {
+  console.log("time callback result: "+code);
+  console.log("sent value: "+value.toString('hex'));
+}
+
 var TimeCharacteristic = function() {
   TimeCharacteristic.super_.call(this, {
     uuid: 'ec0f',
@@ -13,7 +18,8 @@ var TimeCharacteristic = function() {
     value: null
   });
   this._value = new Buffer.alloc(0);
-  this._updateValueCallback = null;
+  // this._updateValueCallback = null;
+  this._updateValueCallback = time_cb;
 };
 
 util.inherits(TimeCharacteristic, BlenoCharacteristic);
@@ -24,8 +30,11 @@ TimeCharacteristic.prototype.onReadRequest = function(offset, callback) {
   this._value.writeUInt32LE(parseInt(time/1000));
   this._value.writeUInt32LE(parseInt(time%1000), 4);
   console.log('TimeCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
-
+  console.log('callback', callback)
+  // setTimeout(() => {  callback(this.RESULT_SUCCESS, this._value);}, 200);
+  // await new Promise(resolve => setTimeout(resolve, 50));
   callback(this.RESULT_SUCCESS, this._value);
+
 };
 
 
