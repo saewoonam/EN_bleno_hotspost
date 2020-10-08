5ef6fa8d851acce761ac75bfcf16b503ecfb0026
Sae Woo <nams@nist.gov>
2020-10-08 15:57:01 -0600

    Tweak to use different versions of phin

--- read_file.js
+++ read_file.js
@@ -17,27 +17,55 @@ let chunk;
 let skip = 0;
 let count = 0;
 
+async function upload_block(block) {
+    let encounters = block.map(d => {
+        return {
+            clientKey: d.client_key,
+            timestamp: d.timestamp,
+            _meta: d
+        }
+    })
+    // console.log(encounters);
+    // upload.upload_batch_v1(encounters);
+    // upload.upload_batch_phin2(encounters);
+    try {
+        res = await upload.upload_batch_phin(encounters)
+        console.log(res.body.toString())
+        console.log("uploaded block of ", block.length);
+    } catch(e) {
+        console.log(e)
+    }
+                
+    // console.log(res.body.toString())
+     
+    // upload.upload_batch_phin(encounters)
+    //     .then((res) => {
+    //         console.log(res.body.toString())
+    //     })
+    //     .catch((e) => console.log(e));
+
+}
 readable.on('readable', () => {
     while (null !== (chunk = readable.read(32))) {
-        if (skip>0) {
+        if (skip>0) { // skip header info
             skip -= 1;
             // console.log(skip,': ', chunk.toString('hex'))
         } else if (Buffer.compare(zeros, chunk)==0) {
-            skip = 2;
+            skip = 2; // set number of 32byte chunks to skip because it is header info
             // console.log(skip, ': ', chunk.toString('hex'))
             part_one = true;
         } else {
             // console.log('start part_one', part_one)
             if (part_one) {
-                chunk.copy(row_buffer, 0, 0);
+                chunk.copy(row_buffer, 0, 0);  // copy first 32bytes of event info
                 // console.log("part one", chunk.toString('hex'))
                 part_one = false;
             } else {
-                chunk.copy(row_buffer, 32, 0);
+                chunk.copy(row_buffer, 32, 0); // copy 2nd 32 bytes of event info
                 // console.log("part two", chunk.toString('hex'))
                 //
                 // Don't process any old data if it is there...
-                if((row_buffer[63]==0)
+                if((row_buffer[63]==0)  // this is v1 firmware data...
                     &&(row_buffer[62]==0x07 || row_buffer[62]==0x01)) {
                     part_one = true;
                     // console.log("old format");
@@ -47,30 +75,15 @@ readable.on('readable', () => {
                     data = convert.bytesToData(row_buffer);
                     data = data[0]
                     if (data.minute > (1577836800/60)) {
-                        block.push(data);
+                        block.push(data); // append data to block
                         count += 1; 
                     } else {
                         // console.log("too early");
                     }
                     if (count==50) {
-                        let encounters = block.map(d => {
-                            return {
-                                clientKey: d.client_key,
-                                timestamp: d.timestamp,
-                                _meta: d
-                            }
-                        })
-			            // console.log(encounters);
-                        // upload.upload_batch_v1(encounters);
-                        upload.upload_batch_phin2(encounters);
-                        // upload.upload_batch_phin(encounters)
-                        //     .then((res) => {
-                        //         console.log(res.body.toString())
-                        //     })
-                        //     .catch((e) => console.log(e));
-                        console.log("convert block of 50: ", data.timestamp);
-                        count = 0;
+                        upload_block(block);
                         block = [];
+                        count = 0;
                     }
                 }
             }
@@ -79,56 +92,7 @@ readable.on('readable', () => {
     }
 });
 readable.on('close', () => {
-    // console.log('closed ', block.length)
-    let encounters = block.map(d => {
-        return {
-            clientKey: d.client_key,
-            timestamp: d.timestamp,
-            _meta: d
-        }
-    })
-    console.log("convert last block", block.length, ":", encounters[encounters.length-1].timestamp);
-    // console.log(JSON.stringify({"encounters": encounters}));
-    // upload.upload_batch(encounters);
-    // this phin2 works
-    // upload.upload_batch_phin2(encounters);
-    upload.upload_batch_phin(encounters)
-        .then((res) => {
-            console.log(res.body.toString())
-        })
-        .catch((e) => console.log(e));
-    // console.log(encounters)
+    // upload last chunk of data
+    upload_block(block);
 });
 
-if (false) {
-    binary = fs.readFileSync('588e8166afd3',null);
-    console.log(typeof(binary));
-    num_blocks = binary.length/32;
-    for (i=0; i<num_blocks; i++) {
-    // for (i=0; i<10; i++) {
-        if (Buffer.compare(zeros, binary.slice((i<<5), (i<<5)+32))==0) {
-            i += 2;
-        } else {
-            evt = binary.slice(i<<5,(i<<5)+64)
-            evt.copy(row_buffer, 0, 0)
-            // console.log(row_buffer)
-            d = convert.bytesToData(row_buffer);
-            i++;
-        }
-    }
-}
-if (false) {
-        console.log('evt: ' + evt.length)
-        let row = new DataView(evt.buffer)
-        console.log('row: ', row, evt.buffer)
-        minute = row.getUint32(0)
-        console.log('minute: '+minute)
-        let tz_offset_ms = new Date().getTimezoneOffset() * 60 * 1000;
-        let d = new Date(minute * 60 * 1000 - tz_offset_ms) // 3600 * 6 * 1000)
-        let timestamp = d.toISOString()
-        timestamp = timestamp.split('.')
-        timestamp = timestamp[0]
-        entry = {timestamp: timestamp}
-
-        console.log(evt.toString('hex'));
-}
